1903b73b398ee89b45b03390d6888968ce47e233
Haris Semic <hsemic991@gmail.com>
2022-01-31 20:28:25 +0100

    Added server directory to .gitignore

--- src/app/api/askIt.ts
+++ src/app/api/askIt.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://mop-react-backend.herokuapp.com/'
   // headers: { Pragma: 'no-cache' }
 });
